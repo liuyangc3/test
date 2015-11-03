058280fd0290653f17541ff70c5431b142021c99
web <liuyangc3@126.com>
2015-11-03 14:25:44 +0800

    dynamic inventory

--- ansible/inventory.py
+++ ansible/inventory.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# ansible dynamic inventory executable script
+
 import argparse
 import json
 import sys
 
-~                                                   
-# dynamic_inventory
+
+# add  class B/C network ip address
+hosts = []
+for i in xrange(1, 255):
+    for j in xrange(1, 255): 
+        hosts.append("192.168.{0}.{1}".format(i, j))
+        hosts.append("172.16.{0}.{1}".format(i, j))
+        
+inventorys = {"groupname": hosts}
+
 def parse_args():
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
@@ -14,18 +24,15 @@ def parse_args():
     group.add_argument('--host')
     return parser.parse_args()
 
-hosts = {"tomcat": ["172.16.200.83","172.16.200.51"]}
 
-def out(host):
-    info = {"ansible_ssh_port": 22, "ansible_ssh_user": "root"}
-    info["ansible_ssh_host"] = host
-    return info
-
-
+def get_host_info(ip):
+    host_info = {"ansible_ssh_port": 22, "ansible_ssh_user": "root"}
+    host_info["ansible_ssh_host"] = ip
+    return host_info
 
 if __name__ == '__main__':
     args = parse_args()
     if args.list:
-        json.dump(hosts, sys.stdout)
+        json.dump(inventorys, sys.stdout)
     if args.host:
-        json.dump(out(args.host), sys.stdout)
+        json.dump(get_host_info(args.host), sys.stdout)
