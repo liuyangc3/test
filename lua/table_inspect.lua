5420df2e1f245f8c133e394a6248be2b24fc6a6d
web <liuyangc3@126.com>
2016-01-22 09:47:05 +0800

    Create table_inspect.lua

---
+++ lua/table_inspect.lua
@@ -0,0 +1,48 @@
+function print_r ( t )
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+
+t = { x = 0, fuck = "you", f=function() return 10 end, "no key", t={"hello"} }
+print_r(t)
+
+table: 000000000002b960 {
+  [1] => "no key"
+  [x] => 0
+  [fuck] => "you"
+  [f] => function: 00000000004e0900
+  [t] => table: 000000000002b960 {
+           [1] => "hello"
+         }
+
+
