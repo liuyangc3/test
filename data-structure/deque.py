42f3a8e1b7ab74acacc40baa12933dd3fc4a964e
web <liuyangc33@gmail.com>
2016-02-01 23:08:31 +0800

    Create deque.py

---
+++ data-structure/deque.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*- 
+"""
+Deque : double-ended queue
+
+add to rear -->      +----+----+---+ <-- add to front
+                     |item|item|...|
+remove from rear <-- +----+----+---+ --> remove from front
+"""
+
+http://c2.com/cgi/wiki?DoubleEndedQueue
