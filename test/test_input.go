524ab82ba03ca3fb6c3b65927ddf8df83bddbc84
web <liuyangc33@gmail.com>
2016-06-12 16:18:03 +0800

    Update test_input.go

--- test/test_input.go
+++ test/test_input.go
@@ -1,1 +1,39 @@
 package test
+
+import (
+	"fmt"
+	"github.com/mozilla-services/heka/pipeline"
+)
+
+type TestInputConfig struct {
+	Address  string `toml:"address"`
+	ListName string `toml:"key"`
+	DataBase string `toml:"db"`
+}
+
+type TestInput struct {
+	conf *TestInputConfig
+}
+
+func (test *TestInput) ConfigStruct() interface{} {
+	return &TestInputConfig{"localhost:6379", "heka", 0}
+}
+
+func (test *TestInput) Init(config interface{}) error {
+	test.conf = config.(*TestInputConfig)
+	fmt.Println(test.conf.Address, test.conf.ListName, test.conf.DataBase)
+	return nil
+}
+
+func (test *TestInput) Run(ir pipeline.InputRunner, h pipeline.PluginHelper) error {
+	for pack := range ir.InChan() {
+		pack.Recycle(nil)
+	}
+	return nil
+}
+
+func init() {
+	pipeline.RegisterPlugin("TestInput", func() interface{} {
+		return new(TestInput)
+	})
+}
