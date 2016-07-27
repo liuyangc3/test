62b269ec9049b947fb90a7d9fd831ced704c7727
web <liuyangc33@gmail.com>
2016-07-27 14:04:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,38 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-__author__ = 'web'
+# divide a iter Object into specific groups
 
-import unittest
-from selenium import webdriver
+def divide(data_list, groups):
+    """
+    :param data_list: 带分组数组
+    :param groups: 分组组数
+    """
+    length = len(data_list)
+    if length == 1:
+        return tuple(data_list)
+
+    # 组数应该小于长度
+    if groups > length:
+        groups = length
+
+    num_per_group = length // groups
+    if num_per_group <= length % groups:
+        num_per_group += 1
+
+    slices = []
+    start = 0
+    for i in range(groups):
+        end = start + num_per_group
+        slices.append(slice(start, end))
+        start = end
+    # 最后一次切割应该到data的末尾
+    slices[-1] = slice(start - num_per_group, length)
+    return tuple([data_list[s] for s in slices])
 
 
-class TestUbuntuHomepage(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.PhantomJS('C:\bin\phantomjs.exe')
+print divide(range(8), 3)
+print divide(range(14), 4)
+print divide(range(14), 3)
+print divide(range(14), 2)
 
-    def testTitle(self):
-        self.driver.get('http://zn.t.dbn.cn/login.aspx')
-
-    def tearDown(self):
-        self.driver.quit()
+# groups is bigger than iter
+print divide(range(3), 5)
